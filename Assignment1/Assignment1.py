304325c17b50ec5439fb3f43c85435cbdda45487
Anna4931 <aniketpatel3172@gmail.com>
2024-02-19 21:51:23 +0530

    Clear

---
+++ Assignment1/Assignment1.py
@@ -0,0 +1,7 @@
+"""
+Assignment 1: Word Counter
+Create a Python script that reads a text file and counts the frequency of each word in the file.
+Store the word frequencies in a dictionary.
+Print out the top 5 most common words and their frequencies.
+
+"""
