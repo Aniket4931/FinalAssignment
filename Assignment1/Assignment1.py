95b1bb366440633360c57254f3a991781e07ffec
Anna4931 <aniketpatel3172@gmail.com>
2024-02-20 09:26:21 +0530

    Complete

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -5,3 +5,19 @@ Store the word frequencies in a dictionary.
 Print out the top 5 most common words and their frequencies.
 
 """
+
+a=open("C:/Users/ADMIN/OneDrive/Documents/FinalAssignment/Assignment1/a.txt")
+c=0
+b=dict()
+for l in a:    
+    word=l.split(" ")
+    for words in word:
+        if words in b:
+            b[words]=b[words]+1
+        else:
+            b[words]=1
+      
+l=list(b.keys())
+fi=l[:4]
+for i in fi:
+    print(i,":",b[i])
